ef4ca2b405b6d11bbfc4cf2f99359fc9ef64f160
pkun <936637810@qq.com>
2020-06-25 16:04:36 +0800

    test: 增加Web自动化测试

--- src/main/java/com/example/hotel/blImpl/hotel/RoomServiceImpl.java
+++ src/main/java/com/example/hotel/blImpl/hotel/RoomServiceImpl.java
@@ -3,17 +3,22 @@ package com.example.hotel.blImpl.hotel;
 import com.example.hotel.bl.hotel.RoomService;
 import com.example.hotel.data.hotel.RoomMapper;
 import com.example.hotel.po.HotelRoom;
+import com.example.hotel.util.RedisUtil;
 import com.example.hotel.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.example.hotel.util.RedisUtil.roomKeyNamePrefix;
+
 @Service
 public class RoomServiceImpl implements RoomService {
 
     @Autowired
     private RoomMapper roomMapper;
+    @Autowired
+    RedisUtil redisUtil;
 
     @Override
     public List<HotelRoom> retrieveHotelRoomInfo(Integer hotelId) {
@@ -23,6 +28,7 @@ public class RoomServiceImpl implements RoomService {
     @Override
     public void insertRoomInfo(HotelRoom hotelRoom) {
         roomMapper.insertRoom(hotelRoom);
+        redisUtil.delete(roomKeyNamePrefix+hotelRoom.getHotelId());
     }
 
     @Override
